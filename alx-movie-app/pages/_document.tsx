fe78772f53d411d8e58a0d0a7db206c0bf5baf62
Castro <baffourcastro@gmail.com>
2025-10-25 00:28:56 +0000

    ProWeb Pulse: Mastering PWA Fundamentals

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -3,8 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#0070f3" />
+      </Head>
+
+      <body className="antialiased">
         <Main />
         <NextScript />
       </body>
